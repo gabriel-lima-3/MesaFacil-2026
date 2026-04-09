a6891db95dc8405b26af4338e42f4823aa2eaa06
Gabriel <gabriellimma00@gmail.com>
2026-04-09 19:09:24 -0300

    feat: adiciona extends Jpa para meu Repository

---
+++ src/main/java/com/gabriellima/MesaFacil2026/Repository/ClienteRepository.java
@@ -0,0 +1,8 @@
+package com.gabriellima.MesaFacil2026.Repository;
+
+import com.gabriellima.MesaFacil2026.Model.ClienteModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<ClienteModel, Long> {
+
+}
