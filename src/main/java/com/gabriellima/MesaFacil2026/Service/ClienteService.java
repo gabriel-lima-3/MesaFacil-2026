d83f0bd53a7690b0005254c4037680ddc01f9db7
Gabriel <gabriellimma00@gmail.com>
2026-04-09 19:11:14 -0300

    feat: adiciona ClienteService com logica de Criar novo Cliente, Buscar Por Id, Deletar por Id, Atualizar Cliente por id

---
+++ src/main/java/com/gabriellima/MesaFacil2026/Service/ClienteService.java
@@ -0,0 +1,68 @@
+package com.gabriellima.MesaFacil2026.Service;
+
+import com.gabriellima.MesaFacil2026.Model.ClienteModel;
+import com.gabriellima.MesaFacil2026.Repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    //Retornar todos os clientes
+    public List<ClienteModel> findALl(){
+        return clienteRepository.findAll();
+
+    }
+
+    //Criar novo Cliente
+    public ClienteModel criarCliene(ClienteModel clienteModel){
+
+        return clienteRepository.save(clienteModel);
+
+    }
+
+    //BuscarPorId
+    public Optional<ClienteModel> findById( Long id){
+
+        return clienteRepository.findById(id);
+    }
+
+    //Deletar
+    public void deleteById(Long id){
+
+        if(clienteRepository.existsById(id)){
+            clienteRepository.deleteById(id);
+        }else{
+            throw new RuntimeException("Cliente nao encontrado");
+        }
+    }
+
+    public ClienteModel atualizarCliente(ClienteModel model, Long id){
+
+        Optional<ClienteModel>  clienteExiste = clienteRepository.findById(id);
+        if(clienteExiste.isPresent()) {
+
+            ClienteModel clienteAtual = clienteExiste.get();
+            clienteAtual.setNome(model.getNome());
+            clienteAtual.setEmail(model.getEmail());
+            clienteAtual.setNumero(model.getNumero());
+            return clienteRepository.save(clienteAtual);
+
+        }else{
+            throw  new RuntimeException("Cliente nao encontrado");
+        }
+
+
+
+
+    }
+
+
+
+}
