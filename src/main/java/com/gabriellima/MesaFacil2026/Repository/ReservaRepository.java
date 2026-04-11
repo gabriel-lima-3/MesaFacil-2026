e13174a51207166864cbb074a5e08416885cd11f
Gabriel <gabriellimma00@gmail.com>
2026-04-10 22:00:52 -0300

    feat: cria ReservaRepository com meotodos do Jpa

---
+++ src/main/java/com/gabriellima/MesaFacil2026/Repository/ReservaRepository.java
@@ -0,0 +1,8 @@
+package com.gabriellima.MesaFacil2026.Repository;
+
+import com.gabriellima.MesaFacil2026.Model.ReservaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservaRepository extends JpaRepository<ReservaModel, Long> {
+
+}
